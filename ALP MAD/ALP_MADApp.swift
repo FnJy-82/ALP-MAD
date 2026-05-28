56825005a4efa4b13d6333adb08db9527a305a16
Flabianos Jason <fmuljono@student.ciputra.ac.id>
2026-05-28 08:21:32 +0700

    Initial Commit

---
+++ ALP MAD/ALP_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MADApp.swift
+//  ALP MAD
+//
+//  Created by Emma Puspa Sari on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
