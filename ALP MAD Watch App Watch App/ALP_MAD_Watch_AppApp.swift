6a8e81e503dd168cc60e7cfbfe4a4767a8ab6d98
Flabianos Jason <fmuljono@student.ciputra.ac.id>
2026-06-11 00:55:03 +0700

    chore(watch): scaffold watchOS app target (companion ALP MAD)

---
+++ ALP MAD Watch App Watch App/ALP_MAD_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MAD_Watch_AppApp.swift
+//  ALP MAD Watch App Watch App
+//
+//  Created by Emma Puspa Sari on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MAD_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALP MAD Watch App Watch AppTests/ALP_MAD_Watch_App_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ALP_MAD_Watch_App_Watch_AppTests.swift
+//  ALP MAD Watch App Watch AppTests
+//
+//  Created by Emma Puspa Sari on 11/06/26.
+//
+
+import Testing
+@testable import ALP_MAD_Watch_App_Watch_App
+
+struct ALP_MAD_Watch_App_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
